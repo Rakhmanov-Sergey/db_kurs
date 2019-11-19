f79dc8523ab7423655def976246d24dbded32037
Sergey Rakhmanov <rakhmanov.sergey@gmail.com>
2019-11-19 12:46:04 +0300

    Initial commit. Done with db schemes

---
+++ core/admin.py
@@ -0,0 +1,131 @@
+from django.contrib import admin
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+
+
+from core.models import Song, Event, User, Client, Editor, Manager, Request
+
+
+class UserUpdateForm(forms.ModelForm):
+    password = forms.CharField(required=False, widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("username",)
+
+    def save(self, commit=True):
+        user = super(UserUpdateForm, self).save(commit=False)
+        password = self.cleaned_data["password"]
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
+
+
+class ClientForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        abstract_user_id = kwargs.pop('user_id')
+        abstract_user = User.objects.get(pk=abstract_user_id)
+        self.fields['user_id'].initial = abstract_user
+        super(ClientForm, self).__init__(*args, **kwargs)
+
+
+class EditorForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        abstract_user_id = kwargs.pop('user_id')
+        abstract_user = User.objects.get(pk=abstract_user_id)
+        self.fields['user_id'].initial = abstract_user
+        super(EditorForm, self).__init__(*args, **kwargs)
+
+
+class ManagerForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        abstract_user_id = kwargs.pop('user_id')
+        abstract_user = User.objects.get(pk=abstract_user_id)
+        self.fields['user_id'].initial = abstract_user
+        super(ManagerForm, self).__init__(*args, **kwargs)
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        # if request.user.is_authenticated():
+        is_client = (request.user.type == "client")
+        if is_client:
+            form.base_fields['name'].disabled = True
+
+        return form
+
+    pass
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    # list_display = ("username","first_name", "last_name", "email","is_active","is_staff","last_login","date_joined")
+    forms = UserUpdateForm
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups'), 'classes': ('hidden',)}),
+        ('Important dates', {'fields': ('last_login', 'date_joined'), 'classes': ('hidden',)}),
+        (None, {'fields': ('type',)})
+    )
+
+    def get_form(self, request, obj=None, **kwargs):
+        # self.exclude = ("email", )
+        form = super(UserAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['first_name'].initial = 'Cool'
+        return form
+
+    def response_add(self, request, obj, post_url_continue=None):
+        if obj.type == 'client':
+            return redirect('/core/client/add/?user_id=%s' % obj.id)
+        elif obj.type == 'editor':
+            return redirect('/core/editor/add/?user_id=%s' % obj.id)
+        elif obj.type == 'manager':
+            return redirect('/core/manager/add/?user_id=%s' % obj.id)
+
+    # def response_change(self, request, obj):
+    #     return redirect('/admin/sales/invoice')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    forms = ClientForm
+    fieldsets = (
+        (None, {'fields': ('number', 'card_number', 'address')}),
+        (None, {'fields': ('user_id',), 'classes': ('hidden',)}),
+    )
+
+
+@admin.register(Editor)
+class EditorAdmin(admin.ModelAdmin):
+    forms = EditorForm
+    fieldsets = (
+        (None, {'fields': ('staff_code',)}),
+        (None, {'fields': ('user_id',), 'classes': ('hidden',)}),
+    )
+
+
+@admin.register(Manager)
+class ManagerAdmin(admin.ModelAdmin):
+    forms = ManagerForm
+    fieldsets = (
+        (None, {'fields': ('staff_code',)}),
+        (None, {'fields': ('user_id',), 'classes': ('hidden',)}),
+    )
+
+
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    pass
+
---
+++ core/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    TYPE_CHOICES = (('manager', 'Организатор мероприятия'), ('client', 'Заказчик'), ('editor', 'Музыкальный редактор'))
+    type = models.CharField(max_length=20, blank=True, choices=TYPE_CHOICES)
+
+    def save(self, force_insert=False, force_update=False):
+        self.first_name = self.type
+        super(User, self).save(force_insert, force_update)
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=20)
+    artist = models.CharField(max_length=20)
+    file_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "Song %s for %s" % (self.id, list(map(lambda event: "Event#%s" % event.id, self.event_set.all())))
+
+
+class Event(models.Model):
+    date = models.DateField()
+    host = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    songs = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return "Event#%s" % self.id
+
+
+class SongRights(models.Model):
+    SONG_RIGHTS_CHOICES = (('requested', 'Запрос отправлен'), ('processing', 'В обработке'), ('confirmed', 'Подтверждено'))
+    organization = models.CharField(max_length=50)
+    status = models.CharField(max_length=20, choices=SONG_RIGHTS_CHOICES)
+    song = models.OneToOneField(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Song rights for song %s with state %s" % (self.song, self.status)
+
+
+class Client(models.Model):
+    number = models.CharField(max_length=20, blank=True)
+    card_number = models.CharField(max_length=50, blank=True)
+    address = models.TextField(blank=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Client extension for user#%s" % self.user_id.username
+
+
+class Editor(models.Model):
+    staff_code = models.CharField(max_length=20, blank=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Editor extension for user#%s" % self.user_id.username
+
+
+class Manager(models.Model):
+    staff_code = models.CharField(max_length=20, blank=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Manager extension for user#%s" % self.user_id.username
+
+
+class Request(models.Model):
+    STATE_CHOICES = (('processing', 'В обработке'), ('confirmed', 'Подтверждена'))
+    date = models.DateTimeField()
+    state = models.CharField(max_length=20, choices=STATE_CHOICES)
+    music = models.TextField()
+    event = models.ForeignKey(Event, blank=True, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Request for %s" % self.user.username
